287fac3f5eb091b0649a0beae3a15f2ac20100f7
Mansoor <example@email.com>
2025-09-16 10:12:25 +0200

    Update stored proc to test

--- sql/stored_procedures/usp_test.sql
+++ sql/stored_procedures/usp_test.sql
@@ -2,4 +2,5 @@ CREATE OR ALTER PROCEDURE dbo.usp_test
 AS
 BEGIN
     select 'hello from test one' as column1, 'this is another column for testing' as column2
+    , 'I made more changes to test git' as column3
 END;
