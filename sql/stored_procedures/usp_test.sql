c823841c0f9b6c7c7cbf3467d0d1a5b9c13db9a7
Mansoor <example@email.com>
2025-09-16 10:25:00 +0200

    Even more changes to proc

--- sql/stored_procedures/usp_test.sql
+++ sql/stored_procedures/usp_test.sql
@@ -2,5 +2,5 @@ CREATE OR ALTER PROCEDURE dbo.usp_test
 AS
 BEGIN
     select 'hello from test one' as column1, 'this is another column for testing' as column2
-    , 'I made more changes to test git' as column3
+    , 'I made more changes to test git' as column3, 'even more changes' as column4
 END;
