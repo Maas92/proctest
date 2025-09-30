63fef1332150044f46c2511c90f1da806b0894b0
Your Name <you@example.com>
2025-09-30 08:12:39 +0200

    add column

--- sql/stored_procedures/testproc.sql
+++ sql/stored_procedures/testproc.sql
@@ -3,6 +3,6 @@ AS
 BEGIN
     -- SQL statements that the stored procedure will execute
     -- For example:
-    SELECT 'HelloWorld' as columna
+    SELECT 'HelloWorld' as columna, 'columnb' as columnb;
 END;
 GO
